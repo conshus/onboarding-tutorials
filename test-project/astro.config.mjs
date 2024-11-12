3af340ac10e37fa935874bed023c82aee3176368
Dwane Hemmings <dwane.hemmings@vonage.com>
2024-11-12 15:21:53 -0500

    Add files via upload

---
+++ test-project/astro.config.mjs
@@ -0,0 +1,31 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+import myIntegration from './my-toolbar-app/my-integration.ts';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    myIntegration,
+    starlight({
+      title: 'Vonage Onboarding',
+      tableOfContents: false,
+      pagefind: false,
+      // social: {
+      // 	github: 'https://github.com/withastro/starlight',
+      // },
+      // sidebar: [
+      // 	{
+      // 		label: 'Guides',
+      // 		items: [
+      // 			// Each item here is one entry in the navigation menu.
+      // 			{ label: 'Example Guide', slug: 'guides/example' },
+      // 		],
+      // 	},
+      // 	{
+      // 		label: 'Reference',
+      // 		autogenerate: { directory: 'reference' },
+      // 	},
+      // ],
+    }),
+  ],
+});
